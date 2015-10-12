0acd23a3cdd56eedca5fd48647ce1df5f0716d54
jdevillard <jeremie.devillard@moskitos.fr>
2015-10-13 00:15:18 +0200

    add - powershell script to deploy the template

---
+++ deploy.ps1
@@ -0,0 +1,14 @@
+﻿#Indicate your new ResourceGroupName
+$resourceGroupName = "kong1";
+$subscriptionId = "4a81a3c5-f813-42bf-b6de-3a1c242ade9a";
+$location = "West Europe"
+
+
+Add-AzureAccount
+
+Switch-AzureMode AzureResourceManager
+
+Select-AzureSubscription -SubscriptionId $subscriptionId -Current
+
+New-AzureResourceGroup -ResourceGroupName $resourceGroupName -location $location `
+-TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json
